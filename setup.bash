03855bca93a7a6596b1d440dfea6a5610f4f5c5b
laurynas <unextbusinessman@gmail.com>
2024-02-17 13:47:50 +0100

    Small fix

--- setup.bash
+++ setup.bash
@@ -7,11 +7,10 @@ sudo mv micro /usr/bin/
 
 sudo apt install -yy git docker docker-compose docker.io zsh
 sudo usermod -aG docker $USER
-newgrp docker
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions docker)/" ~/.zshrc
 sudo -k chsh -s $(which zsh) $USER
 
-docker network create proxy
+# docker network create proxy
